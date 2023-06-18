10b42be8b9cbc495eff0ce078ea4853c8d61465f
Ye Lwin Htay <yelwin.r.k@gmail.com>
2023-06-18 15:36:50 -0700

    git ignore

--- Sort/radixSort.js
+++ Sort/radixSort.js
@@ -15,6 +15,20 @@ function mostDigits(arr) {
     return count
 }
 
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums);
+    for (let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({length: 10}, () => []) // [ [], [], [], [], [], [], [], [], [], [] ]
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k)
+            digitBuckets[digit].push(nums[i])
+        }
+        nums = [].concat(...digitBuckets)
+    }
+    return nums
+}
+
 // console.log(getDigit(75432, 3)) //5
 // console.log(digitCount(4300)) //4
 // console.log(mostDigits([303, 101313, 11, 8, 5, 7, 3, 11])) // 6
+console.log(radixSort([30, 10, 11, 808, 5, 7890, 3, 11]))
