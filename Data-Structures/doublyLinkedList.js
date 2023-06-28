4e28587ecbfbb4d8c02229ccecf3f4e6ad17aced
Ye Lwin Htay <yelwin.r.k@gmail.com>
2023-06-27 19:37:24 -0700

    update doublyLinkedList

---
+++ Data-Structures/doublyLinkedList.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor (val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++
+        return this
+
+    }
+}
